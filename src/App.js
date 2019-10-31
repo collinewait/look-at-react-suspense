273e70861a9efffef0c160716fefdbe7fbe01163
collinewait <collinewait17@gmail.com>
2019-10-31 12:09:24 +0300

    loade person details from api and a ramdom number

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
+import { createResource } from "./person-api";
+import { Person } from "./Person";
+import { Num } from "./Num";
+
+const resource = createResource();
 
 function App() {
-  return <div>Hey</div>;
+  return (
+    <div className="App">
+      <div>Another name here</div>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Num resource={resource} />
+        <Person resource={resource} />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
