809eed17311f67cfe8b6356dd900515d03c9c340
collinewait <collinewait17@gmail.com>
2019-10-31 12:16:18 +0300

    add resource to the state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import "./App.css";
 import { createResource } from "./person-api";
 import { Person } from "./Person";
 import { Num } from "./Num";
 
-const resource = createResource();
+const initialResource = createResource();
 
 function App() {
+  const [resource, setResource] = useState(initialResource);
   return (
     <div className="App">
       <div>Another name here</div>
