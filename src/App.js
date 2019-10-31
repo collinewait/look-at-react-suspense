c3169559ca4f55a18002273b39cee296729c355a
collinewait <collinewait17@gmail.com>
2019-10-31 12:50:59 +0300

    handling errors with ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 import { createResource } from "./person-api";
 import { Person } from "./Person";
 import { Num } from "./Num";
+import { ErrorBoundary } from "./ErrorBoundary";
 
-// const initialResource = createResource();
+const initialResource = createResource();
 
 function App() {
-  const [resource, setResource] = useState(() => createResource());
+  // const [resource, setResource] = useState(() => createResource());
+  const [resource, setResource] = useState(initialResource);
   return (
     <div className="App">
       <div>Another name here</div>
       <Suspense fallback={<h1>Loading person...</h1>}>
-        <Person resource={resource} />
+        <ErrorBoundary>
+          <Person resource={resource} />
+        </ErrorBoundary>
       </Suspense>
       <Suspense fallback={<h1>Loading num...</h1>}>
         <Num resource={resource} />
